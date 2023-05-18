76bff1141c7731e1ad16f59e2ae626c7c122379a
Хлестов Андрей <andrei-h-74@mail.ru>
2023-05-18 17:06:27 +0300

    add model

--- api_yamdb/api/models.py
+++ api_yamdb/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Сategories(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.slug
